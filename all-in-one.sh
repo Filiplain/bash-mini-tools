b5e7d2cb7f73da33acda4bab454290f1c45399eb
Filiplain <85502601+Filiplain@users.noreply.github.com>
2021-06-19 18:47:44 -0400

    Update all-in-one.sh

--- all-in-one.sh
+++ all-in-one.sh
@@ -21,22 +21,33 @@ if [ "$1" ]
 then
 echo -e "${blue}\nScanning TCP ports${end} \n"
 allTCPports=$(nmap -Pn --min-rate 5000 -p- $1|grep 'tcp' |cut -d '/' -f 1 > tcp.tmp;cat tcp.tmp|tr '\n' ',')
-echo -e "\n${blue}Open TCP ports:${end}${yellow} \n $(echo $allTCPports| tr ',' '\n')${end}\n\n"
-echo -e "\n${blue}Full Scan for Open TCP ports:\n${end}"
-nmap -Pn -sV -sC -p$allTCPports $1 > tcp.txt
-cat tcp.txt
-rm tcp.tmp
-
+  
+  if [ -s ./tcp.tmp ]
+    then
+    echo -e "\n${blue}Open TCP ports:${end}${yellow} \n $(echo $allTCPports| tr ',' '\n')${end}\n\n"
+    echo -e "\n${blue}Full Scan for Open TCP ports:\n${end}"
+    nmap -Pn -sV -sC -p$allTCPports $1 > tcp.txt
+    cat tcp.txt
+    rm tcp.tmp
+  else
+      echo -e "${red}\n\n -- No TCP ports open -- \n\n${end}"  
+  fi
   if [ $(id -u) == "0" ]
   then
        echo -e "\n ${blue}Scanning UDP ports ${end}\n"
        udpports=$(nmap -Pn -sU --min-rate 5000 --open $1|grep 'udp' |cut -d '/' -f 1 > udp.tmp;cat udp.tmp|tr '\n' ',')
-       echo -e "\n${blue}Open UDP ports:${end}${yellow} \n $(echo $udpports| tr ',' '\n')${end}\n\n"
-       echo -e "\n${blue}Full Scan for Open UDP ports:\n${end}"
-       nmap -Pn -sU -sV -sC -p$udpports $1 > udp.txt
-       cat udp.txt
-       rm udp.tmp
-
+       if [ -s ./udp.tmp ]
+         then    
+         echo -e "\n${blue}Open UDP ports:${end}${yellow} \n $(echo $udpports| tr ',' '\n')${end}\n\n"
+         echo -e "\n${blue}Full Scan for Open UDP ports:\n${end}"
+         nmap -Pn -sU -sV -sC -p$udpports $1 > udp.txt
+         cat udp.txt
+         rm udp.tmp
+       else
+          echo -e "${red}\n\n -- No UDP ports open -- \n\n${end}"
+       		
+       
+       fi
 
   else
      echo -e "${red}\n For UDP scan you need root privileges.\n Usage: sudo $0 <IP address>${end}"
@@ -50,4 +61,3 @@ else
 fi
 
 echo -e "\n\n\n${red}Made${end} in ${blue}Do${end}"
-
