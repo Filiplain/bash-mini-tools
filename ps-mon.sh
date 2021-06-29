1b0db9ea5529ea97667d55829ebec605b68cb9db
Filiplain <85502601+Filiplain@users.noreply.github.com>
2021-06-29 11:42:27 -0400

    Update ps-mon.sh

--- ps-mon.sh
+++ ps-mon.sh
@@ -8,12 +8,12 @@ end="\033[0m\e[0m"
 red="\e[0;31m\033[1m"
 blue="\e[0;34m\033[1m"
 yellow="\e[0;33m\033[1m"
-
+filename=$(date +"%m%s")
 trap ctrl_c INT
 
 function ctrl_c(){
 echo -e "\n\n\n${red}Exiting...${end}"
-echo -e "\n\n\n${blue}Out File:${end} /tmp/newproc.txt"
+echo -e "\n\n\n${blue}Out File:${end} /tmp/$filename.txt"
 echo -e "\n\n\n${red}Made${end} in ${blue}Do${end}"
 exit 0
 }
@@ -24,13 +24,14 @@ for n in $(seq 1 1000)
 do
         echo -e "${blue}\nRunning each${end} ${yellow}$seconds${end} ${blue}seconds:${end} $n${red}/${end}1000\n\n"
 	ps2=$(ps -eo command)
-        diff <(echo "$ps1") <(echo "$ps2") >> /tmp/newproc.txt 
+        diff <(echo "$ps1") <(echo "$ps2") >> /tmp/$filename.txt 
         ps1=$ps2
-	tail /tmp/newproc.txt
+	tail /tmp/$filename.txt
         sleep $seconds
 	clear
         
 
 done
-echo -e "\n\n\n${blue}Out File:${end} /tmp/newproc.txt"
+echo -e "\n\n\n${blue}Out File:${end} /tmp/$filename.txt"
 echo -e "\n\n\n${red}Made${end} in ${blue}Do${end}"
+
